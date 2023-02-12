42b5758e4752c9a7b3b1a58f22ff08773b4578ad
LJVK <jvklnp@live.com>
2023-02-11 22:59:36 -0800

    added "high_score" and file system logic

--- snake_game/main.py
+++ snake_game/main.py
@@ -25,16 +25,23 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
+    # Detect collision with the wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        is_game_on = False
-        scoreboard.game_over()
+        # is_game_on = False
+        # scoreboard.game_over()
+        snake.reset()
+        scoreboard.reset()
+    # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            is_game_on = False
-            scoreboard.game_over()
+            # is_game_on = False
+            # scoreboard.game_over()
+            snake.reset()
+            scoreboard.reset()
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,6 +7,8 @@ FONT = ('Arial', 16, 'normal')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.score = 0
         self.color("white")
         self.penup()
@@ -15,14 +17,22 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Your Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Your Score: {self.score} High Score = {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", mode='w') as file:
+            file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over", move=False, align=ALIGNMENT, font=FONT)
