bd8140c4f62efbf9e1f983fa1f65a4a60a6ce85d
LJVK <jvklnp@live.com>
2023-02-18 13:40:35 -0800

    new program

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -7,6 +7,7 @@ with open("./Input/Letters/starting_letter.txt") as letter_files:
     letter_contents = letter_files.read()
     for name in names:
         stripped_name = name.strip()
+        print(stripped_name)
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
---
+++ us-states-game/main.py
@@ -0,0 +1,31 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+file_data = pandas.read_csv("50_states.csv")
+all_states = file_data.state.to_list()
+guessed_state = []
+states_missed = []
+
+while len(guessed_state) < 50:
+    user_input = screen.textinput(title=f"{len(guessed_state)}/50 State Correct", prompt="What's another state name?").title()
+    if user_input == "Exit":
+        for state in all_states:
+            if state not in guessed_state:
+                states_missed.append(state)
+        new_data = pandas.DataFrame(states_missed)
+        new_data.to_csv("states_to_be_learned.csv")
+        break
+    if user_input in all_states:
+        guessed_state.append(user_input)
+        state_data = file_data[file_data.state == user_input]
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
